b7b4f4efdccfc154aceda05c25de9c39ca52fe0c
Rohan Malik <rohanmalik801@gmail.com>
2026-06-02 11:59:58 +0530

    Fix CORS for all Vercel URLs

--- server/index.js
+++ server/index.js
@@ -9,7 +9,21 @@ connectDB()
 
 const app = express()
 
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true }))
+app.use(cors({
+  origin: function(origin, callback) {
+    const allowed = [
+      'http://localhost:5173',
+      'https://medicare-azure-six.vercel.app',
+      'https://medicare-git-main-rohanmalik801-8636s-projects.vercel.app'
+    ]
+    if (!origin || allowed.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true
+}))
 app.use(express.json())
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
